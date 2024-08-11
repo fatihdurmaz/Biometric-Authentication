41b04e925dbab489289589ff813cb1a99ff4dde7
Fatih DURMAZ <fatih.durmaz@outlook.com>
2024-08-11 22:33:16 +0300

    Initial Commit

---
+++ Biometric Authentication/Biometric_AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Biometric_AuthenticationApp.swift
+//  Biometric Authentication
+//
+//  Created by Fatih Durmaz on 11.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Biometric_AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
